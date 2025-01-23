9f87c4dd047156caa419f9f4283f904bf4d1416e
ipcloudflaretamiz <mmdviruos@gmail.com>
2025-01-23 18:36:21 +0330

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -6,30 +6,18 @@ echo "=== https://github.com/ipcloudflaretamiz ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
-
-# Download the file
-wget https://download.mikrotik.com/routeros/7.17/all_packages-arm-7.17.zip -O chr.img.zip || { echo "Download failed"; exit 1; }
-
-# Unzip the file instead of using gunzip
-unzip chr.img.zip || { echo "Extraction failed"; exit 1; }
-
-# Locate the storage device
-STORAGE=$(lsblk | grep disk | awk '{print $1}' | head -n 1)
-echo "STORAGE is $STORAGE"
-
-# Get network information
-ETH=$(ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p')
-echo "ETH is $ETH"
-ADDRESS=$(ip addr show $ETH | grep global | awk '{print $2}' | head -n 1)
-echo "ADDRESS is $ADDRESS"
-GATEWAY=$(ip route list | grep default | awk '{print $3}')
-echo "GATEWAY is $GATEWAY"
-
-# Wait before writing to disk
-sleep 5
-
-# Write the image to disk
-dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync || { echo "Disk write failed"; exit 1; }
-echo "Ok, reboot"
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
+wget https://download.mikrotik.com/routeros/7.17/netinstall64-7.17.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
+echo STORAGE is $STORAGE && \
+ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
+echo ETH is $ETH && \
+ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
+echo ADDRESS is $ADDRESS && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo GATEWAY is $GATEWAY && \
+sleep 5 && \
+dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
+echo "Ok, reboot" && \
+echo 1 > /proc/sys/kernel/sysrq && \
+echo b > /proc/sysrq-trigger && \
